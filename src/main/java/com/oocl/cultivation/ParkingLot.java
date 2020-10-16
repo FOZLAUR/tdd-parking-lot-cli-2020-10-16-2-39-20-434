46120ba6c5e929dac9f81904378de0d95979f571
FOZLAUR <fozla@oocl.com>
2020-10-16 14:19:26 +0800

    [parking-lot-tdd][LaurenceFoz] feature: Added logic for park function inside Parking Lot

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -3,7 +3,8 @@ package com.oocl.cultivation;
 import java.util.Map;
 
 public class ParkingLot {
+
     public ParkingTicket park(Car car) {
-        return null;
+        return new ParkingTicket();
     }
 }
