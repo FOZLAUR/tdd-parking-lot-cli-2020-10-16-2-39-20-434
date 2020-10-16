14673d3e3062f6713547e250712884ea0ab0a34c
FOZLAUR <fozla@oocl.com>
2020-10-16 23:48:30 +0800

    [parking-lot-tdd][LaurenceFoz] feature: Added feature for park in ParkingBoy to accommodate the new List variable that contains all of the Parking Lots assigned to Parking Boy

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,23 +1,31 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ParkingBoy {
     private ParkingLot parkingLot;
+    private List<ParkingLot> parkingLotList = new ArrayList<>();
 
     public ParkingBoy(ParkingLot parkingLot){
         this.parkingLot = parkingLot;
     }
 
     public ParkingBoy(List<ParkingLot> parkingLotList){
-
+        this.parkingLotList = parkingLotList;
     }
 
     public ParkingTicket park(Car car) {
-        return parkingLot.park(car);
+        List<ParkingLot> parkingLotsWithPosition = parkingLotList.stream().filter(parkingLot -> !parkingLot.isFull()).collect(Collectors.toList());
+        return parkingLotsWithPosition.size() == 0 ? parkingLotList.get(0).park(car) : parkingLotsWithPosition.get(0).park(car);
     }
 
     public Car fetchCar(ParkingTicket parkingTicket) {
         return parkingLot.fetchCar(parkingTicket);
+    }
+
+    public void printParkingLots(){
+        parkingLotList.stream().forEach(parkingLot -> System.out.println(parkingLot.getMapSize()));
     }
 }
