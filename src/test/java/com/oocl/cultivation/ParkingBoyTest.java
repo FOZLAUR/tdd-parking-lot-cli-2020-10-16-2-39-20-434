92bfc65426784e0daec08147fc5767d236de4888
FOZLAUR <fozla@oocl.com>
2020-10-16 14:33:38 +0800

    [parking-lot-tdd][LaurenceFoz] test: Added test case for Given two tickets when fetch car then return the corresponding cars

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -25,4 +25,19 @@ public class ParkingBoyTest {
 
         assertSame(car, fetchedCar);
     }
+
+    @Test
+    public void should_return_two_cars_when_fetch_car_given_two_ticket() {
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        ParkingTicket parkingTicket1 = parkingBoy.park(car1);
+        ParkingTicket parkingTicket2 = parkingBoy.park(car2);
+        Car fetchedCar1 = parkingBoy.fetchCar(parkingTicket1);
+        Car fetchedCar2 = parkingBoy.fetchCar(parkingTicket2);
+
+        assertSame(car1, fetchedCar1);
+        assertSame(car2, fetchedCar2);
+    }
 }
