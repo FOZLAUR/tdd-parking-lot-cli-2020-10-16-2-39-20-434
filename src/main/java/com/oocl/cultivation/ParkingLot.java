03c57924639f3d0d71409ab1f4b62631aebabcc2
FOZLAUR <fozla@oocl.com>
2020-10-16 14:29:22 +0800

    [parking-lot-tdd][LaurenceFoz] feature: Added Map implementation to Parking Lot class to store Parking Ticket and Car; added logic for fetchCar

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,14 +1,18 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
+    private Map<ParkingTicket, Car> parkingTicketCarMap = new HashMap<>();
 
     public ParkingTicket park(Car car) {
-        return new ParkingTicket();
+        ParkingTicket parkingTicket = new ParkingTicket();
+        parkingTicketCarMap.put(parkingTicket, car);
+        return parkingTicket;
     }
 
     public Car fetchCar(ParkingTicket parkingTicket) {
-        return null;
+        return parkingTicketCarMap.get(parkingTicket);
     }
 }
