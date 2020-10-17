f6d9132f4da0abfbdda6e38fbca629be433f3366
FOZLAUR <fozla@oocl.com>
2020-10-17 19:31:23 +0800

    [parking-lot-tdd][LaurenceFoz] test/refactor: Added Test Case for Story 5; Edited name of Test Case for Story 4 to be more descriptive

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -157,7 +157,7 @@ public class ParkingBoyTest {
     }
 
     @Test
-    public void should_return_park_in_lot_with_more_capacity_when_parking_2_cars_given_two_parking_lots_with_capacity_1_and_capacity_2() {
+    public void should_return_park_in_lot_with_more_capacity_when_parking_2_cars_given_smart_parking_boy_two_parking_lots_with_capacity_1_and_capacity_2() {
         //given
         List<ParkingLot> parkingLotList = new ArrayList<>();
         ParkingLot parkingLot1 = new ParkingLot(1);
@@ -175,5 +175,29 @@ public class ParkingBoyTest {
         //then
         assertTrue(parkingLotList.get(1).containsTicket(parkingTicket1));
         assertTrue(parkingLotList.get(0).containsTicket(parkingTicket2));
+    }
+
+    @Test
+    public void should_park_in_lot_with_higher_position_rate_when_parking_3_cars_given_super_smart_parking_boy_two_parking_lots_with_capacity_2_and_capacity_3() {
+        //given
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(2);
+        ParkingLot parkingLot2 = new ParkingLot(3);
+        parkingLotList.add(parkingLot1);
+        parkingLotList.add(parkingLot2);
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLotList);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+
+        //when
+        ParkingTicket parkingTicket1 = superSmartParkingBoy.park(car1);
+        ParkingTicket parkingTicket2 = superSmartParkingBoy.park(car2);
+        ParkingTicket parkingTicket3 = superSmartParkingBoy.park(car3);
+
+        //then
+        assertTrue(parkingLotList.get(0).containsTicket(parkingTicket1));
+        assertTrue(parkingLotList.get(1).containsTicket(parkingTicket2));
+        assertTrue(parkingLotList.get(1).containsTicket(parkingTicket3));
     }
 }
