b29dfd4b83f26972991bcc37596dfc21d254c27e
FOZLAUR <fozla@oocl.com>
2020-10-19 20:28:07 +0800

    [parking-lot-tdd][LaurenceFoz] feature: Added logic of throwing exceptions in SmartParkingBoy class

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,5 +1,9 @@
 package com.oocl.cultivation;
 
+import com.oocl.exceptions.CarIsAlreadyParkedException;
+import com.oocl.exceptions.CarIsNullException;
+import com.oocl.exceptions.ParkingLotOutOfPositionsException;
+
 import java.util.List;
 
 public class SmartParkingBoy extends ParkingBoy{
@@ -15,6 +19,13 @@ public class SmartParkingBoy extends ParkingBoy{
                 .reduce((p1,p2) -> {
                     return p1.getRemainingSpaces() < p2.getRemainingSpaces() ? p2 : p1 ;
                 }).orElse(null);
+        if (car==null){
+            throw new CarIsNullException();
+        } else if(isCarParkedInAnyParkingLots(car)){
+            throw new CarIsAlreadyParkedException();
+        } else if (parkingLotWithPosition==null) {
+            throw new ParkingLotOutOfPositionsException();
+        }
         return parkingLotWithPosition == null ? parkingLotList.get(0).park(car) : parkingLotWithPosition.park(car);
     }
 }
